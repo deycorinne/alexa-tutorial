86c24f60e3136d45e2edcbc29efb0aa8e45418d1
Corinne Dey <cjdey@pbs.org>
2021-05-10 11:45:47 -0400

    more handlers

--- lambda/src/index.js
+++ lambda/src/index.js
@@ -15,6 +15,36 @@ const LaunchRequestHandler = {
   }
 }; 
 
+const HelpIntentHandler = {
+  canHandle(handlerInput) {
+      return alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest'
+          && alexa.getIntentName(handlerInput.requestEnvelope) === 'AMAZON.HelpIntent';
+  },
+  handle(handlerInput) {
+      const speakOutput = 'You can say hello to me! How can I help?';
+
+      return handlerInput.responseBuilder
+          .speak(speakOutput)
+          .reprompt(speakOutput)
+          .getResponse();
+  }
+};
+
+const ErrorHandler = {
+  canHandle() {
+      return true;
+  },
+  handle(handlerInput, error) {
+      const speakOutput = 'Sorry, I had trouble doing what you asked. Please try again.';
+      console.log(`~~~~ Error handled: ${JSON.stringify(error)}`);
+
+      return handlerInput.responseBuilder
+          .speak(speakOutput)
+          .reprompt(speakOutput)
+          .getResponse();
+  }
+};
+
 /**
  * This handler acts as the entry point for your skill, routing all request and response
  * payloads to the handlers above. Make sure any new handlers or interceptors you've
@@ -29,8 +59,9 @@ const LaunchRequestHandler = {
         .withSkillId(process.env.ASK_SKILL_ID)
         .addRequestHandlers( 
             LaunchRequestHandler, 
+            HelpIntentHandler
         )
-        //.addErrorHandlers(ErrorHandler)
+        .addErrorHandlers(ErrorHandler)
         .create();
     
     const response = await skill.invoke(event, context);
